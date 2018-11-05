a24a289f2aa9880c3e921f646a1b697debb7c01e
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-11-05 17:56:41 -0500

    checking solution for css stuff

--- src/index.js
+++ src/index.js
@@ -1,19 +1,64 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
+'use strict'
+document.addEventListener("DOMContentLoaded", () => {
 
-// YOUR CODE HERE
+	fetchToys()
 
-addBtn.addEventListener('click', () => {
-  // hide & seek with the form
-  addToy = !addToy
-  if (addToy) {
-    toyForm.style.display = 'block'
-    // submit listener here
-  } else {
-    toyForm.style.display = 'none'
-  }
-})
 
+	const addBtn = document.querySelector("#new-toy-btn");
+	let addToy = false;
+	addBtn.addEventListener("click", (e) => addToyDrawer(e, addToy));
+});
+
+let addToyDrawer = function(e, addToy) {
+	const toyForm = document.querySelector(".container");
+	// hide & seek with the form
+	addToy = !addToy;
+	if (addToy) {
+		toyForm.style.display = "block";
+		// submit listener here
+	} else {
+		toyForm.style.display = "none";
+	}
+};
+
+function getToyDiv() {
+	return document.querySelector('#toy-collection');
+}
+
+
+
+function fetchToys() {
+	fetch('http://localhost:3000/toys')
+		.then(resp => resp.json())
+		.then(json => {
+			let collection = getToyDiv();
+			json.forEach(toy => {
+				collection.appendChild(renderToy(toy));
+			})
+		})
+
+}
+
+function renderToy(toy) {
+	debugger
+	let card = document.createElement('div');
+	card.className = 'card';
+
+	let nameEl = document.createElement('h2');
+	nameEl.innerText = toy.name;
+
+	let imgEl = document.createElement('img');
+	imgEl.src = toy.image;
+	imgEl.className = 'toy-avatar';
+
+	let likesEl = document.createElement('p');
+	likesEl.innerText = toy.likes;
+
+	let likeBtn = document.createElement('button');
+	likeBtn.className = 'like-btn';
+
+	card.append(...[nameEl, imgEl, likesEl, likeBtn]);
+	return card;
+}
 
 // OR HERE!
